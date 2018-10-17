1945a594a927d2b26712a0c1aa72ccda40e9dd20
Anu Khambete <anukhambete@gmail.com>
2018-10-17 00:49:55 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,6 +24,7 @@ class StudentsController < ApplicationController
   def index
     binding.pry
     @students = Student.all
+    @search = Student.search(params[:term])
   end
 
   def student_params
